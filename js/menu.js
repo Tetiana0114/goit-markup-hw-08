3930eee47363844e24562a5beb156f270c14aa15
Tetiana Kotliarova <t.tanya0114@gmail.com>
2022-06-01 12:06:54 +0300

    Mobile menu new edits

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open-btn'),
-    closeMenuBtn: document.querySelector('.menu-close-btn'),
-    menu: document.querySelector('.mob-menu'),
-    body: document.querySelector('body'),
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
